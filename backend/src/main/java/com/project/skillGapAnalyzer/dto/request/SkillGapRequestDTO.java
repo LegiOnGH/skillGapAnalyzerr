331124c174bb601857c8729a73adb44ce9a18fa1
LegiOnGH <rohitthakur438a@gmail.com>
2026-04-30 10:41:42 +0530

    allowed users to analyze with no skills

--- backend/src/main/java/com/project/skillGapAnalyzer/dto/request/SkillGapRequestDTO.java
+++ backend/src/main/java/com/project/skillGapAnalyzer/dto/request/SkillGapRequestDTO.java
@@ -11,9 +11,8 @@ import java.util.List;
 @Data
 public class SkillGapRequestDTO {
 
-    @NotEmpty(message = "Skills cannot be empty")
     @Size(max = 15, message = "You can select up to 15 skills")
-    private List<@NotBlank(message = "Skill cannot be blank") String> userSkills;
+    private List<String> userSkills;
 
     @NotBlank(message = "Target role is required")
     @Size(max = 50, message = "Target role too long")
