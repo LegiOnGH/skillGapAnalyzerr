1c1bba925424a3d0a7dabe39abc138cdce1af14e
LegiOnGH <rohitthakur438a@gmail.com>
2026-05-18 00:53:53 +0530

    corrected import

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState} from "react";
-import { useAuthStore } from "../store/authStore";
+import useAuthStore from "../store/authStore";
 import { useMe, useUpdateMe } from "../features/profile/hooks";
 import { getErrorMessage } from "../utils/errorHandler";
 
